607036cc0a5c6bfd27325ba8d54c8724b874dcdc
Adria Arque <adria.arque@enti.cat>
2023-11-21 10:24:00 +0100

    Primeros pasos en el cliente/servidor del protocolo EFTP

---
+++ eftp/server.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+CLIENT="localhost"
+
+echo "Servidor de EFTP"
+
+echo "(0) Listen"
+
+DATA=`nc -l -p 3333 -w 0`
+
+echo $DATA
+
+echo "(3) Test & Send" #comprobar si lo que ha llegado es igual a la cabecera "EFTP 1.0"
+
+if [ "$DATA" != "EFTP 1.0" ]
+then
+	echo "ERROR 1: BAD HEADER"
+	sleep 1
+	echo "KO_HEADER" | nc $CLIENT 3333
+	exit 1
+fi
+
+echo "OK_HEADER"
+sleep 1
+echo "OK_HEADER" | nc $CLIENT 3333 #envío
+
+echo "(4) Listen"
+
+DATA=`nc -l -p 3333 -w 0` #paso 4 escuchar
+
+echo $DATA
+
+echo "(7) Test & Send" #paso 7
+
+if [ "$DATA" != "BOOOM" ] #paso 7
+then
+	echo "ERROR 2: BAD HANDSHAKE"
+	sleep 1
+	echo "KO_HANDSHAKE" | nc $CLIENT 3333
+	exit 2
+fi
+
+echo "OK_HANDSHAKE"
+sleep 1
+echo "OK_HANDSHAKE" | nc $CLIENT 3333
+
+echo "(8) Listen"
+
+DATA=`nc -l -p 3333 -w 0`
