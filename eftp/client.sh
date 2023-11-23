112ceae448a80fd78091555da1978ee71fc7a498
Adria Arque <adria.arque@enti.cat>
2023-11-23 13:51:20 +0100

    FILE_NAME: Send&Check

--- eftp/client.sh
+++ eftp/client.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+IP=`ip address | grep inet | grep -i enp0s3 | cut -d " " -f 6 | cut -d "/" -f 1`
+
+echo $IP
+
+SERVER="localhost"
+
 echo "Cliente de EFTP"
 
 echo "(1) Send"
 
-echo "EFTP 1.0" | nc localhost 3333
+echo "EFTP 1.0" | nc $SERVER 3333
 
 echo "(2) Listen"
 
@@ -22,7 +28,7 @@ fi
 
 echo "BOOOM"
 sleep 1
-echo "BOOOM" | nc localhost 3333
+echo "BOOOM" | nc $SERVER 3333
 
 echo "(6) Listen"
 
@@ -30,5 +36,20 @@ DATA=`nc -l -p 3333 -w 0` #paso 6 escuchar
 
 echo $DATA
 
+echo "(9) Test"
 
+if [ "$DATA" != "OK_HANDSHAKE" ]
+then
+	echo "ERROR 2: BAD HANDSHAKE"
+	exit 2
+fi
+
+echo "(10) Send"
+
+sleep 1
+
+echo "FILE_NAME fary1.txt" | nc $SERVER 3333
+
+echo "(11) Listen"
+DATA=`nc -l -p 3333 -w 0`
 
--- eftp/server.sh
+++ eftp/server.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-CLIENT="10.65.0.69"
-PORT="3333"
+CLIENT="localhost"
 
 echo "Servidor de EFTP"
 
 echo "(0) Listen"
 
-DATA=`nc -l -p $PORT -w 0`
+DATA=`nc -l -p 3333 -w 0`
 
 echo $DATA
 
@@ -17,17 +16,17 @@ if [ "$DATA" != "EFTP 1.0" ]
 then
 	echo "ERROR 1: BAD HEADER"
 	sleep 1
-	echo "KO_HEADER" | nc $CLIENT $PORT
+	echo "KO_HEADER" | nc $CLIENT 3333
 	exit 1
 fi
 
 echo "OK_HEADER"
 sleep 1
-echo "OK_HEADER" | nc $CLIENT $PORT #envío
+echo "OK_HEADER" | nc $CLIENT 3333 #envío
 
 echo "(4) Listen"
 
-DATA=`nc -l -p $PORT -w 0` #paso 4 escuchar
+DATA=`nc -l -p 3333 -w 0` #paso 4 escuchar
 
 echo $DATA
 
@@ -37,14 +36,28 @@ if [ "$DATA" != "BOOOM" ] #paso 7
 then
 	echo "ERROR 2: BAD HANDSHAKE"
 	sleep 1
-	echo "KO_HANDSHAKE" | nc $CLIENT $PORT
+	echo "KO_HANDSHAKE" | nc $CLIENT 3333
 	exit 2
 fi
 
 echo "OK_HANDSHAKE"
 sleep 1
-echo "OK_HANDSHAKE" | nc $CLIENT $PORT
+echo "OK_HANDSHAKE" | nc $CLIENT 3333
 
 echo "(8) Listen"
 
-DATA=`nc -l -p $PORT -w 0`
+DATA=`nc -l -p 3333 -w 0`
+
+echo "(12) Test&Store&Send"
+
+PREFIX=`echo $DATA | cut -d " " -f 1`
+
+if [ "$PREFIX" != "FILE_NAME" ]
+then
+	echo "ERROR 3: BAD FILE NAME PREFIX"
+	sleep 1
+	echo "KO_FILE_NAME" | nc $CLIENT 3333
+	exit 3
+fi
+
+FILE_NAME=`echo $DATA | cut -d " " -f 2` #cortamos con el delimitador que es el espacio y nos quedamos con la 2a columna que es el nombre
